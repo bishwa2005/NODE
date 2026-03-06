2e7bfa6ac498b6f5aeb9a284367ce430aa40acd9
bishwa2005 <br180905@gmail.com>
2026-03-06 16:04:56 +0530

    post api

--- 24_post api mysql/index.js
+++ 24_post api mysql/index.js
@@ -20,11 +20,11 @@ app.get("/data",(req,res)=>{
 
 app.post("/",(req,res)=>{
     const data = {"username":"pparker","email":"peter@midtown.edu","password":"$2y$10$Z3xV5c7B8"}
-    con.query("insert into users values", data, (error,result,fields)=>{
+    con.query("insert into users SET ?", data, (error,result,fields)=>{
         if(error){
             throw error;
         }
-        res.send(result);
+        res.send(result); 
     })
 
 })
