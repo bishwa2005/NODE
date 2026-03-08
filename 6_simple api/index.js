0fff6c47d9e7258a0a8bf3ce3467c3f6f24f6b09
bishwa2005 <br180905@gmail.com>
2026-03-08 18:53:01 +0530

    about api

--- 6_simple api/index.js
+++ 6_simple api/index.js
@@ -5,3 +5,5 @@ http.createServer((req,res)=>{
     res.write(JSON.stringify({name:'bishwa',age:'21',contact:"123456779"}));
     res.end();
 }).listen(5000)
+
+// An API (Application Programming Interface) is a set of rules and protocols that allow different software programs to communicate with each other and exchange data. It acts as an intermediary or a "messenger," taking requests from one application (the client) to another application (the server) and delivering a response, without the developer needing to know the internal workings of the other system
